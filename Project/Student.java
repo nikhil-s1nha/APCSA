5ae4539757f128668026be524166598e5bca28a6
Nikhil <namnik100@gmail.com>
2026-02-19 09:54:13 -0800

    project

---
+++ Project/Student.java
@@ -0,0 +1,15 @@
+package Project;
+
+public class Student{
+    private String name;
+    private double grade;
+    private int numSumbitted;
+
+    public Student(String name, double grade, int numSubmitted){
+        this.name = name;
+        this.grade = grade;
+        this.numSubmitted = numSubmitted;
+    }
+
+    
+}
